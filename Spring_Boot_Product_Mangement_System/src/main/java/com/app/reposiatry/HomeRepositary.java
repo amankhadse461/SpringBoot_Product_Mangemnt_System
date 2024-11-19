4c793951e94e3ed8ca4ce177af7c30d665a16549
amankhadse461 <amankhadse461@gmail.com>
2024-11-19 22:01:27 +0530

    Extends JpaRepository for database operations

--- Spring_Boot_Product_Mangement_System/src/main/java/com/app/reposiatry/HomeRepositary.java
+++ Spring_Boot_Product_Mangement_System/src/main/java/com/app/reposiatry/HomeRepositary.java
@@ -1,5 +1,9 @@
 package com.app.reposiatry;
 
-public interface HomeRepositary {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.model.Product;
+
+public interface HomeRepositary  extends JpaRepository<Product, Integer>{
 
 }
